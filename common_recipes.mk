053caa81238402579116c959b3ec0a930aed647b
Hyeokjin Doo <tyeirdoo06@gmail.com>
2023-01-06 04:32:28 +0900

    add rules for test

--- common_recipes.mk
+++ common_recipes.mk
@@ -1,11 +1,29 @@
 SRC_MAIN = main.cpp
+TEST_NAME ?= check
 
 SRC_IMP += ../utils/BigBeautifulTestFrame.cpp
 ifeq ($(V), 1)
 	DEFINE = -D VERBOSE
 endif
 
-SRC = $(SRC_IMP) $(SRC_MAIN)
+TEST_MAIN = test.cpp
+
+SRC = $(SRC_IMP)
+
+INCLUDE = -I ../utils
+
+CFLAGS = -Wall -Wextra -Werror --std=c++98 -MMD -MP
+
+ifeq ($(MAKECMDGOALS), test)
+	SRC += $(TEST_MAIN)
+	INCLUDE += -I ../utils/doctest/doctest
+	CFLAGS = -Wall -Wextra -Werror --std=c++17 -MMD -MP
+else ifeq ($(MAKECMDGOALS), clean)
+	SRC += $(TEST_MAIN)
+	SRC += $(SRC_MAIN)
+else
+	SRC += $(SRC_MAIN)
+endif
 
 DEP = $(SRC:.cpp=.d)
 
@@ -13,9 +31,6 @@ OBJ = $(SRC:.cpp=.o)
 
 JSON= $(SRC:.cpp=.part.json)
 
-CFLAGS = -Wall -Wextra -Werror --std=c++98 -MMD -MP
-
-INCLUDE = -I ../utils
 
 COMPILECOMMAND = compile_commands.json
 
@@ -44,6 +59,7 @@ clean :
 fclean :
 	$(MAKE) clean
 	rm -f $(NAME)
+	rm -f $(TEST_NAME)
 	rm -f $(COMPILECOMMAND)
 
 re :
@@ -52,6 +68,13 @@ re :
 
 dev : 
 	(printf [ && find . -name "*.part.json" | xargs cat && printf ]) > compile_commands.json
+
+test : $(TEST_NAME)
+
+$(TEST_NAME) : $(OBJ)
+	$(CXX) $(CFLAGS) $(TEST_INCLUDE) $(INCLUDE) $(OBJ) -o $@
+
+.PHONY : all clean fclean re dev test
 
 ifneq "$(MAKECMDGOALS)" "clean"
 -include $(DEP)
